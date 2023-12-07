74c2864f5576aad085e73e7e78d01861d1dc9dc7
zhtmr <zhtmr123@gmail.com>
2023-12-07 21:13:00 +0900

    .

--- src/main/java/org/example/boj/Boj_2739.java
+++ src/main/java/org/example/boj/Boj_2739.java
@@ -1,9 +1,18 @@
 package org.example.boj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Boj_2739 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    int input = Integer.parseInt(br.readLine());
+    for (int i = 1; i < 10; i++) {
+      System.out.printf("%d * %d = %d\n", input, i, input * i);
+    }
 
   }
 

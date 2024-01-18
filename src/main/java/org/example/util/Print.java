64b04b830f5c1047184b697f4d93c89c76d12eb7
csw <susu199037@gmail.com>
2024-01-18 20:09:30 +0900

    .

---
+++ src/main/java/org/example/util/Print.java
@@ -0,0 +1,31 @@
+package org.example.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Print {
+  public static void main(String[] args) throws IOException {
+    folder("./src/main/java/", 1);
+  }
+
+  static File folder(String path, int level) throws IOException {
+    File dir = new File(path);
+    File[] files = dir.listFiles();
+    for (File f : Objects.requireNonNull(files)) {
+      System.out.print(indent(level));
+
+      if (f.isDirectory() && !f.isHidden()) {
+        System.out.printf("%s/\n", f.getName());
+        folder(f.getCanonicalPath(), level + 1);
+      } else if (f.isFile()) {
+        System.out.printf("|-- %s\n", f.getName());
+      }
+    }
+    return null;
+  }
+
+  static String indent(int level) {
+    return "   ".repeat(Math.max(0, level));
+  }
+}

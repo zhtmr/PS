3b2166fb0929d7168b029f0655b384421f73fc14
csw <susu199037@gmail.com>
2024-01-21 19:21:58 +0900

    .

---
+++ src/main/java/org/example/boj/Boj_1085.java
@@ -0,0 +1,71 @@
+package org.example.boj;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
+public class Boj_1085 {
+
+  static FastReader scan = new FastReader();
+  static StringBuilder sb = new StringBuilder();
+
+  public static void main(String[] args) {
+    input();
+  }
+
+  static void input() {
+    int x = scan.nextInt();
+    int y = scan.nextInt();
+    int w = scan.nextInt();
+    int h = scan.nextInt();
+    System.out.println(min(min(abs(w - x), abs(h - y)), min(x, y)));
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public FastReader(String s) throws FileNotFoundException {
+      br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+
+}
